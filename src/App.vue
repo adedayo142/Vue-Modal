bab2e46be6eda63cbbcb0b0f61a17850f2d7c978
onipedadedayo142 <adedayo142@gmail.com>
2022-03-06 15:31:34 +0100

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <div id="nav">
+<div class="first">
+<h1>Vuejs App using slot to make a Modal</h1>
+</div>
+<p>Welcome....</p>
+<div v-if="showModal" class="mode">
+  <Modal theme="" @close="toggleModal">
+     <template v-slot:links>
+       <a href="#">sign up now</a>
+       <a href="#">more info</a>
+    </template>
+    <h1>Give away</h1>
+    <p>Grab your copy now</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo" class="mode">
+  <Modal @close="toggleModalTwo">
+    <h1>Sign up to the newsletter</h1>
+    <p>For updates and promo codes!</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal2</button>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  </div> -->
+  <!-- <router-view/> -->
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Modal from './components/Modal.vue'
+  export default {
+    components:{
+      Modal
+    },
+    data(){
+      return{
+        title: " Hello world",
+        header: "Sign up guys",
+        text: "Grab your copy now",
+        showModal: false,
+        showModalTwo: false
+      }
+    },
+    methods:{
+       toggleModal(){
+         this.showModal = !this.showModal
+      },
+      toggleModalTwo(){
+        this.showModalTwo = !this.showModalTwo
+      }
     }
   }
+</script>
+<style>
+.first{
+  text-align: center;
+}
+.mode{
+  align-content: center;
+}
+h1{
+  text-align: center;
+
+}
+p{
+  text-align: center;
 }
 </style>
